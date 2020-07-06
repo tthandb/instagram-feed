b728176cca87a191f20888a2ba6687be0980a480
tthandb <tthandb@hotmail.com>
2020-07-07 04:29:38 +0700

    done milestone 3

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          backgroundColor: "pink",
+        }}
+      >
+        <View style={{ width: 27 }} />
+        <Image
+          source={{
+            uri:
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png",
+          }}
+          style={{
+            flex: 1,
+            height: 40,
+            resizeMode: "contain",
+          }}
+        />
+        <AntDesign name="rightcircleo" size={27} color="black" />
+      </View>
+      <View style={{ height: 300 }}>
+        <Image
+          source={require("./assets/1.jpg")}
+          style={{ flex: 1, width: null, height: null }}
+          resizeMode="cover"
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
